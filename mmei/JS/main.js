0ef48ec300e474119d89ca3f261da3dbd7f654dd
Marcos Á. Medina <33690842+MarkMed@users.noreply.github.com>
2018-12-07 13:23:07 -0300

    Script added
    Nav bar appears when scroll down

--- mmei/JS/main.js
+++ mmei/JS/main.js
@@ -1,7 +1,13 @@
 $(document).ready(()=>{
-
-    $("#wlcmCover h2").click((ev)=>{
-        console.log(ev.target);
+    function isTop(){
+        var actualTop=$(document).scrollTop();
+        var boolvar;
+        (actualTop>($("#wlcmCover").height())/4)?(boolvar=true):(boolvar=false);
+        return boolvar;
+    }
+    (isTop())?($("#banner").addClass("topFixed")):($("#banner").addClass("topExpanded"));
+    $(document).scroll(()=>{
+        (isTop())?( ($("#banner").hasClass("topExpanded"))?($("#banner").removeAttr("class").addClass("topFixed")):(false) ):($("#banner").removeAttr("class").addClass("topExpanded"));
     });
  
  });
