d061f3982763c61128faa68f3a4eec37e2c4300f
Marcos Á. Medina <33690842+MarkMed@users.noreply.github.com>
2019-03-22 15:48:35 -0300

    smooth parallax effect added
    effect applied in header

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,12 @@
 $(document).ready(()=>{
+	$(document).scroll(function(event){
+		$("header").css(
+			{
+				"opacity": 1-($(document).scrollTop()/800),
+				"transform": "translateY(-"+$(document).scrollTop()/4+"px)"
+			}
+		);
+	});
 	$("#openMenu").change(()=>{
 		($("#openMenu").prop('checked'))?($("body").css("overflow", "hidden")):($("body").css("overflow", ""));
 	});
