22f3cf911f4362f0a125c40fe6b56fb2a0032d83
MarkMed <marcmedi1@hotmail.com>
2019-03-23 18:41:30 -0300

    commit 2 test
    Lets test scroll fx in mobile

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,36 @@
 $(document).ready(()=>{
-	$(document).scroll(function(event){
+	// let lastScrollTop = 0;
+	let chapters=$('[class="chapter"]');
+	$('html, body').animate(
+		{
+			scrollTop: $(chapters[0]).offset().top
+		},
+		400
+	);
+	// let currentChapter=chapters[0];
+	// function changeSection(){
+	// 	if ($(this).scrollTop() > lastScrollTop){
+				
+	// 	}
+	// 	else {
+	// 				// upscroll code
+	// 	}
+	// 	lastScrollTop = $(this).scrollTop();
+		// $('html, body').animate(
+		// 	{
+		// 		scrollTop: $(currentChapter).offset().top
+		// 	},
+		// 	500,
+		// 	'linear'
+		// );
+	// }
+	$(document).scroll(function(e){
+		// e.preventDefault()
+		// changeSection();
+		// chapters.map((elemIndex)=>{
+		// 	console.log(chapters[elemIndex]);
+		// })
+		
 		$("header").css(
 			{
 				"opacity": 1-($(document).scrollTop()/800),
@@ -9,5 +40,13 @@ $(document).ready(()=>{
 	});
 	$("#openMenu").change(()=>{
 		($("#openMenu").prop('checked'))?($("body").css("overflow", "hidden")):($("body").css("overflow", ""));
+	});
+	$("#svgScrollDown").click(()=>{
+		$('html, body').animate(
+			{
+				scrollTop: $(chapters[1]).offset().top
+			},
+			400
+		);
 	});
 });
