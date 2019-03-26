7159805ba316eb4df5c3919c49c72bb0b05aaa5d
Marcos Á. Medina <33690842+MarkMed@users.noreply.github.com>
2019-03-26 14:44:41 -0300

    tips
    A commentary with a link and a (undending) file of ajax

--- registPayment/JS/script.js
+++ registPayment/JS/script.js
@@ -2,6 +2,7 @@ $(document).ready(()=>{
 	()=>{
         $.ajax(
             {
+                type: get,
                 url: "../Resources/regis.json", 
                 success: function(result){
                     $("main").html(result);
