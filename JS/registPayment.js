51d7f61663aa7bdb79c03e1c7114e3ba4afba72f
Marcos Á. Medina <33690842+MarkMed@users.noreply.github.com>
2019-04-01 10:39:39 -0300

    Ajax test
    I need upload json files to server to make right request

--- JS/registPayment.js
+++ JS/registPayment.js
@@ -1,18 +1,14 @@
 $(document).ready(()=>{
 	let xhr= new XMLHttpRequest();
 
-    xhr.open('get', 'Resources/regis.txt', true);
-    xhr.send();
-
-    console.log(xhr.readystate);
-    console.log(xhr.status);
-
     xhr.onreadystatechange = function() {
         console.log("xhr.onreadystatechange");
         if(this.readyState == 4 && this.status == 200) {
-            console.log(xhr.readystate);
-            console.log(xhr.status);
-            console.log(xhr.responsetext);
+            console.log(this.readystate);
+            console.log(this.status);
+            console.log(this.responsetext);
         }
     }
+    xhr.open('get', 'Resources/regis.txt', true);
+    xhr.send();
 });
