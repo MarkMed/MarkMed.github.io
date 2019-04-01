031813a20db6cd6dc42fd84f7f873ccac973664c
Marcos Á. Medina <33690842+MarkMed@users.noreply.github.com>
2019-04-01 11:57:36 -0300

    No time
    I must to be quick, no time to explain

--- JS/registPayment.js
+++ JS/registPayment.js
@@ -4,6 +4,7 @@ $(document).ready(()=>{
     xhr.onreadystatechange = function() {
         console.log("xhr.onreadystatechange");
         if(this.readyState == 4 && this.status == 200) {
+            console.log(this);
             console.log(this.readyState);
             console.log(this.status);
             console.log(this.responseText);
