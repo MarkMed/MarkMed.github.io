365bd89daf9989ad79a8649f588545f776e30afd
MarkMed <marcmedi1@hotmail.com>
2019-04-13 23:29:33 -0300

    i dunno wht 2 say

--- JS/registPayment.js
+++ JS/registPayment.js
@@ -1,16 +1,26 @@
 $(document).ready(()=>{
-	let xhr= new XMLHttpRequest();
+	let ajaxRQS= new XMLHttpRequest();
 
-    xhr.onreadystatechange = function() {
-        console.log("xhr.onreadystatechange");
-        if(this.readyState == 4 && this.status == 200) {
-            console.log(this);
-            console.log(this.readyState);
-            console.log(this.status);
-            console.log(this.responseText);
-            $("section#target").html(this.responseText)
-        }
-    }
-    xhr.open('get', 'https://markmed.github.io/Resources/regis.json', true);
-    xhr.send();
+	const nameInput = $("#userName");
+	const passInput = $("#userPass");
+
+
+	function checkRegisteredUser(userList){
+		for (var i = userList.length - 1; i >= 0; i--) {
+			console.log(userList[i]);
+		}
+
+	}
+	$("#log").click(()=>{
+		let Rresponse;
+		ajaxRQS.onreadystatechange = function() {
+			if(this.readyState == 4 && this.status == 200){
+				console.log(this.response);
+				Rresponse=JSON.parse(ajaxRQS.response);
+				console.log(Rresponse.users);
+			}
+		}
+		ajaxRQS.open('get', 'https://markmed.github.io/Resources/regis.json', true);
+		ajaxRQS.send();
+	});
 });
