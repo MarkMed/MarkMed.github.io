3cf3a8058051a60096850e6059c52c9464278b8a
Marcos Á. Medina <33690842+MarkMed@users.noreply.github.com>
2019-06-24 12:59:24 -0300

    preventDefault added to each handler, changes in html structure, and more

--- touchEvents/JS/script.js
+++ touchEvents/JS/script.js
@@ -10,12 +10,18 @@ $(document).ready(()=>{
 			}
 		);
 	});
-	
+	let header = $("header");
+	const firstSeparator = $("div.lineSeparator:first");
 	$(document).scroll(function(e){		
-		$("header").css(
+		header.css(
 			{
 				"opacity": 1-($(document).scrollTop()/700),
 				"transform": "translateY("+$(document).scrollTop()/2+"px)"
+			}
+		);
+		firstSeparator.css(
+			{
+				"transform": "translateY("+$(document).scrollTop()/8+"px)"
 			}
 		);
 	});
@@ -24,28 +30,32 @@ $(document).ready(()=>{
 	let touchEventsDiv = $("#touchEvents");
 	let touchMoveDiv = $("#touchMove")
 	/////////////////////////////////////////
-	touchStartDiv.on("touchstart", ()=>{
+	touchStartDiv.on("touchstart", (ev)=>{
+		ev.preventDefault();
 		touchStartDiv.html(`Touch Start!`)
 	});
 	/////////////////////////////////////////
 	touchEndDiv.on("touchend", (ev)=>{
+		ev.preventDefault();
 		touchEndDiv.html(`Touch End!`)
 	});
 	/////////////////////////////////////////
 	touchMoveDiv.on("touchmove", (ev)=>{
+		ev.preventDefault();
 		touchMoveDiv.html(`Moving in: <br/> x: ${ev.touches[0].clientX} y:  ${ev.touches[0].clientY}`)
 	});
 	/////////////////////////////////////////
-	touchEventsDiv.on("touchstart", ()=>{
+	touchEventsDiv.on("touchstart", (ev)=>{
+		ev.preventDefault();
 		touchEventsDiv.html(`Touch Start!`)
 	});
-	touchEventsDiv.on("touchend", ()=>{
+	touchEventsDiv.on("touchend", (ev)=>{
+		ev.preventDefault();
 		touchEventsDiv.html(`Touch End!`)
 	});
 	touchEventsDiv.on("touchmove", (ev)=>{
+		ev.preventDefault();
 		touchEventsDiv.html(`Moving in: <br/> x: ${ev.touches[0].clientX} y:  ${ev.touches[0].clientY}`)
 	});
+	/////////////////////////////////////////
 });
-// function functionTest(){
-// 	$("#touchStart").html("Touched1!")
-// }
