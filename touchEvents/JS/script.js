3bed899af24e2b8f7d06a7f030c00f343a587e70
Marcos Á. Medina <33690842+MarkMed@users.noreply.github.com>
2019-06-26 10:59:06 -0300

    smooth animation in title when scroll added

--- touchEvents/JS/script.js
+++ touchEvents/JS/script.js
@@ -2,7 +2,7 @@ $(document).ready(()=>{
 	$("#svgScrollDown").click(()=>{
 		$('html, body').animate(
 			{
-				scrollTop: $("main").offset().top
+				scrollTop: $("#touchEventsTests").offset().top
 			},
 			{
 				duration: 400, 
@@ -10,20 +10,21 @@ $(document).ready(()=>{
 			}
 		);
 	});
-	let header = $("header");
-	const firstSeparator = $("div.lineSeparator:first");
-	$(document).scroll(function(e){		
-		header.css(
-			{
-				"opacity": 1-($(document).scrollTop()/700),
-				"transform": "translateY("+$(document).scrollTop()/2+"px)"
-			}
-		);
-		firstSeparator.css(
-			{
-				"transform": "translateY("+$(document).scrollTop()/8+"px)"
-			}
-		);
+	const header = $("header");
+	const firstTitle = $("#touchEventsTests .sectionTitle h2");
+	let deviceHeight = $(window).height();
+	$(document).scroll(function(e){
+		if($(document).scrollTop() < deviceHeight){
+			header.css(
+				{
+					"opacity": 1-($(document).scrollTop()/700),
+					"transform": "translateY("+$(document).scrollTop()/2+"px)"
+				}
+			);
+		}
+		if($(document).scrollTop() > deviceHeight/2){
+			firstTitle.attr("class", "show");
+		}
 	});
 	let touchStartDiv = $("#touchStart");
 	let touchEndDiv = $("#touchEnd");
@@ -58,4 +59,54 @@ $(document).ready(()=>{
 		touchEventsDiv.html(`Moving in: <br/> x: ${ev.touches[0].clientX} y:  ${ev.touches[0].clientY}`)
 	});
 	/////////////////////////////////////////
+	// // Swipe Up / Down / Left / Right
+	// var initialX = null;
+	// var initialY = null;
+	
+	// function startTouch(e) {
+	// 	initialX = e.touches[0].clientX;
+	// 	initialY = e.touches[0].clientY;
+	// };
+	
+	// function moveTouch(e) {
+		
+	// 	e.preventDefault();
+
+	// 	if (initialX === null) {
+	// 		return;
+	// 	}
+		
+	// 	if (initialY === null) {
+	// 		return;
+	// 	}
+		
+	// 	var currentX = e.touches[0].clientX;
+	// 	var currentY = e.touches[0].clientY;
+		
+	// 	var diffX = initialX - currentX;
+	// 	var diffY = initialY - currentY;
+		
+	// 	if (Math.abs(diffX) > Math.abs(diffY)) {
+	// 		// sliding horizontally
+	// 		if (diffX > 0) {
+	// 		// swiped left
+	// 		console.log("swiped left");
+	// 		} else {
+	// 		// swiped right
+	// 		console.log("swiped right");
+	// 		}  
+	// 	} else {
+	// 		// sliding vertically
+	// 		if (diffY > 0) {
+	// 		// swiped up
+	// 		console.log("swiped up");
+	// 		} else {
+	// 		// swiped down
+	// 		console.log("swiped down");
+	// 		}  
+	// 	}
+
+	// 	initialX = null;
+	// 	initialY = null;
+	// };
 });
