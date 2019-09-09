cedda19ddd23588666412fee7d1d5df1c5f0ee22
Marcos Á. Medina <33690842+MarkMed@users.noreply.github.com>
2019-09-09 10:52:44 -0300

    Drag and Drop events added (must be fix)

--- drag-and-drop/JS/script.js
+++ drag-and-drop/JS/script.js
@@ -36,195 +36,37 @@ $(document).ready(()=>{
 		// }
 	});
 	//////////////////////////////////////////////////////////////////////////////////
-	///// Double Tap Event /////
-	let dblTap= new CustomEvent("dblTap", { bubbles: true });
-	function makeDblTapable(elem){
-		function doubleTap(elem){
-			const bool= (elem.attr("db-tap")==="true")?(true):(false);
-			elem.attr("db-tap", true);
-			setTimeout( function() { elem.attr("db-tap", false) }, 500 );
-			if(bool) {
-				elem.get(0).dispatchEvent(dblTap);
-				return false;
-			}
-			event.preventDefault();
-		}
-		elem.on("touchstart", (ev)=>{
-			ev.preventDefault();
-			elem.attr("db-tap", (!!elem.attr("db-tap"))?((elem.attr("db-tap")==="true")?(true):(false)):(false));
-			doubleTap(elem);
+	
+	function allowDrop(ev) {
+		ev.preventDefault();
+	}
+	
+	function drag(ev) {
+		console.log(ev.target);
+		ev.dataTransfer.addElement(ev.target);
+	}
+	
+	function drop(ev) {
+		ev.preventDefault();
+		console.log(ev);
+		var data = ev.dataTransfer.getData("DOMString");
+		$(ev.target).append(document.getElementById(data));
+	}
+	let cloudDiv = $("#cloudDiv");
+	let storageDiv = $("#storageDiv");
+	let items = storageDiv.children();
+	for(let i=0; i<items.length; i++){
+		$(items[i]).on("dragstart", ()=>{
+			drag(event);
+			$(items[i]).css({backgroundColor: "#ffe", borderLeft: "5px solid #ccc" })
 		});
 	}
-	//////////////////////////////////////////////////////////////////////////////////
+	cloudDiv.on("dragover", ()=>{
+		allowDrop(event);
+	})
+	cloudDiv.on("drop", ()=>{
+		drop(event);
+	})
 
 
-	function makeSwipable(elem){
-		///// Swipe Event /////
-		let swipe;
-		///// Swipe Up Event /////
-		let swipeUp = new CustomEvent("swipeUp", { bubbles: true });
-		///// Swipe Down Event /////
-		let swipeDown = new CustomEvent("swipeDown", { bubbles: true });
-		///// Swipe Rigth Event /////
-		let swipeRight = new CustomEvent("swipeRight", { bubbles: true });
-		///// Swipe Left Event /////
-		let swipeLeft = new CustomEvent("swipeLeft", { bubbles: true });
-		
-		elem.on("touchstart", startTouch);
-		elem.on("touchmove", moveTouch);
-
-		var initialX = null;
-		var initialY = null;
-		
-		function startTouch(e) {
-			initialX = e.touches[0].clientX;
-			initialY = e.touches[0].clientY;
-		};
-		
-		function moveTouch(e) {
-			
-			e.preventDefault();
-
-
-			if (initialX === null) {
-				return;
-			}
-			
-			if (initialY === null) {
-				return;
-			}
-			
-			var currentX = e.touches[0].clientX;
-			var currentY = e.touches[0].clientY;
-			
-			var diffX = initialX - currentX;
-			var diffY = initialY - currentY;
-
-			swipe = new CustomEvent("swipe", { 
-				bubbles: true, 
-				detail:{
-					"swipeAmountX": -diffX,
-					"swipeAmountY": -diffY
-				}
-			});
-			
-			e.target.dispatchEvent(swipe);
-
-			if (Math.abs(diffX) > Math.abs(diffY)) {
-				// sliding horizontally
-				if (diffX > 0) {
-					// swiped left
-					e.target.dispatchEvent(swipeLeft);
-				} else {
-					// swiped right
-					e.target.dispatchEvent(swipeRight);
-				}  
-			} else {
-				// sliding vertically
-				if (diffY > 0) {
-					// swiped up
-					e.target.dispatchEvent(swipeUp);
-				} else {
-					// swiped down
-					e.target.dispatchEvent(swipeDown);
-				}  
-			}
-			initialX = null;
-			initialY = null;
-		};
-	}
-
-	//////////////////////////////////////////////////////////////////////////////////
-	/////////////////////////////////////////
-	let touchStartDiv = $("#touchStart");
-	touchStartDiv.on("touchstart", (ev)=>{
-		ev.preventDefault();
-		touchStartDiv.html(`Touch Start!`)
-	});
-	/////////////////////////////////////////
-	let touchEndDiv = $("#touchEnd");
-	touchEndDiv.on("touchend", (ev)=>{
-		ev.preventDefault();
-		touchEndDiv.html(`Touch End!`)
-	});
-	/////////////////////////////////////////
-	let touchMoveDiv = $("#touchMove");
-	touchMoveDiv.on("touchmove", (ev)=>{
-		ev.preventDefault();
-		touchMoveDiv.html(`Moving in: <br/> x: ${ev.touches[0].clientX} y:  ${ev.touches[0].clientY}`)
-	});
-	/////////////////////////////////////////
-	let touchEventsDiv = $("#touchEvents");
-	touchEventsDiv.on("touchstart", (ev)=>{
-		ev.preventDefault();
-		touchEventsDiv.html(`Touch Start!`)
-	});
-	touchEventsDiv.on("touchmove", (ev)=>{
-		ev.preventDefault();
-		touchEventsDiv.html(`Moving in: <br/> x: ${ev.touches[0].clientX} y:  ${ev.touches[0].clientY}`)
-	});
-	touchEventsDiv.on("touchend", (ev)=>{
-		ev.preventDefault();
-		touchEventsDiv.html(`Touch End!`)
-	});
-	/////////////////////////////////////////
-	const doubleTap = $("#doubleTap");
-	makeDblTapable(doubleTap);
-	/*Listening new events and doing something*/
-	doubleTap.on("dblTap", () => {
-		doubleTap.html(`Double Tap!`)
-	});
-	/////////////////////////////////////////
-	const swipeUp = $("#swipeUp");
-	makeSwipable(swipeUp);
-	swipeUp.on("swipeUp", () => {
-		swipeUp.html("Swiped Up!");
-	});
-	/////////////////////////////////////////
-	const swipeDown = $("#swipeDown");
-	makeSwipable(swipeDown);
-	swipeDown.on("swipeDown", () => {
-		swipeDown.html("Swiped Down!");
-	});
-	/////////////////////////////////////////
-	const swipeRight = $("#swipeRight");
-	makeSwipable(swipeRight);
-	swipeRight.on("swipeRight", () => {
-		swipeRight.html("Swiped Right!");
-	});
-	/////////////////////////////////////////
-	const swipeLeft = $("#swipeLeft");
-	makeSwipable(swipeLeft);
-	swipeLeft.on("swipeLeft", () => {
-		swipeLeft.html("Swiped Left!");
-	});
-	/////////////////////////////////////////
-	const swipeEvent = $("#swipeEvent");
-	swipeEvent.css(
-		{
-			"transition": "0.4s cubic-bezier(0.17, 0.99, 0.88, 1.32)"
-		}
-	);
-	makeSwipable(swipeEvent);
-	swipeEvent.on("swipe", (ev) => {
-		
-		swipeEvent.css(
-			{
-				"transform": "translate("+ev.detail.swipeAmountX+"px, "+ev.detail.swipeAmountY+"px)"
-			}
-		);
-	});
-	swipeEvent.on("swipeUp", () => {
-		swipeEvent.html("Swiped Up!");
-	});
-	swipeEvent.on("swipeDown", () => {
-		swipeEvent.html("Swiped Down!");
-	});
-	swipeEvent.on("swipeRight", () => {
-		swipeEvent.html("Swiped Right!");
-	});
-	swipeEvent.on("swipeLeft", () => {
-		swipeEvent.html("Swiped Left!");
-	});
-	/////////////////////////////////////////
 });
