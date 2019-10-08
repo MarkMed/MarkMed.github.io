c0c8e1197aedae21047cdcf5b556fc989d016a6e
Marcos Á. Medina <33690842+MarkMed@users.noreply.github.com>
2019-10-08 14:41:48 -0300

    Checking if is a touchable device try 2

--- drag-and-drop/JS/script.js
+++ drag-and-drop/JS/script.js
@@ -122,11 +122,23 @@ $(document).ready(()=>{
 		ev.preventDefault();
 	}
 
-	function isTouchDevice() {
-		return !!window.ontouchstart;
-	}
+	function is_touch_device() {
+		var prefixes = ' -webkit- -moz- -o- -ms- '.split(' ');
+		var mq = function(query) {
+		  return window.matchMedia(query).matches;
+		}
+	  
+		if (('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch) {
+		  return true;
+		}
+	  
+		// include the 'heartz' as a way to have a non matching MQ to help terminate the join
+		// https://git.io/vznFH
+		var query = ['(', prefixes.join('touch-enabled),('), 'heartz', ')'].join('');
+		return mq(query);
+	  }
 	
-	alert("The device is touchable: "+isTouchDevice());
+	alert("The device is touchable: "+is_touch_device());
 	let cloudDiv = $("#cloudDiv");
 	let storageDiv = $("#storageDiv");
 	let items = storageDiv.children();
