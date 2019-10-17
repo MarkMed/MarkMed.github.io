e1fcb96d30f5fec1b36e0ea6a99d296065a54138
Marcos Á. Medina <33690842+MarkMed@users.noreply.github.com>
2019-10-17 12:55:51 -0300

    Drag and Drop with data trsnfer finished
    need to add more features, and there is a issue with the css. I will check the SASS compiler in both PC

--- drag-and-drop/JS/DDwithDataTrans.js
+++ drag-and-drop/JS/DDwithDataTrans.js
@@ -7,31 +7,52 @@ $(document).ready(()=>{
     let items = elemStart.children();
 
     //////////////////////////////////////////////////////////////////////////////////
-
-	function allowDrop(ev) {
-		ev.preventDefault();
-    }
-    
-    function dropFunction(ev){
-        allowDrop();
+	
+	function dragEnterFunc(e){
+        e.preventDefault();
+        console.log("Dragging element has entered into the drop target");
         // More things here
+	}
+
+	function dragOverFunc(e){
+		e.preventDefault();
+		e.dataTransfer.dropEffect = "copy";
+        // More things here
+	}
+
+    function dropFunc(e){
+		e.preventDefault();
+		let landingElement = e.target;
+
+		console.log(e.dataTransfer.getData("Text"));
+
+		landingElement.innerHTML += (e.dataTransfer.getData("Text"));
+		
     }
 
-    function dragStartFunction(ev){
-        console.log("DragStart");
+    function dragStartFunc(e){
+		console.log("DragStart");
+
+		let draggingElement = e.target.outerHTML;
+		console.log(e);
+		e.dataTransfer.setData("Text", draggingElement);
     }
 
     function allowDrag(elem){
         elem.setAttribute("draggable", "true");
     }
 
-    //////////////////////////////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////////////////////
+	
+	elemeTarget.get(0).addEventListener("dragenter", dragEnterFunc, false);
+	
+	elemeTarget.get(0).addEventListener("dragover", dragOverFunc);
 
-    elemeTarget.on("drop", dropFunction);
+    elemeTarget.get(0).addEventListener("drop", dropFunc, false);
 
     for(let i=0; i<items.length; i++){
         allowDrag(items[i])
-        $(items[i]).on("dragstart", dragStartFunction);
+        $(items[i]).get(0).addEventListener("dragstart", dragStartFunc, false);
     }
 
 })
