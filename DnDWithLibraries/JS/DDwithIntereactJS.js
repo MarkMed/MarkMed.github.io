dc9b8bd29f5ec2df2b6ad867b3e894a851462275
Marcos Á. Medina <33690842+MarkMed@users.noreply.github.com>
2019-11-15 16:39:54 -0300

    Taking out jQuery code from intercat code

--- DnDWithLibraries/JS/DDwithIntereactJS.js
+++ DnDWithLibraries/JS/DDwithIntereactJS.js
@@ -1,32 +1,21 @@
 var globalVar;
-$(document).ready(()=>{
-	//////////////////////////////////////////////////////////////////////////////////
-	const header = $("header");
-	const firstTitle = $("#DDinSD .sectionTitle");
-	const secondTitle = $("#DDwithDT .sectionTitle");
-	const imgTarget = $("#targetStorage");
-	const cardTarget = $("#elementTarget");
-	const imgArray = $("#startStorage").children();
-	const cardsArray = $("#elementStart .card");
-	let deviceHeight = $(window).height();
-	let documentHeight = $(document).height();
-	//////////////////////////////////////////////////////////////////////////////////
+window.onload=(()=>{
+	const imgTarget = document.querySelector("#targetStorage");
+	const cardTarget = document.querySelector("#elementTarget");
+	const imgArray = document.querySelectorAll("#startStorage img.element2Drag");
+	const cardsArray = document.querySelectorAll("#elementStart div.card");
 
 	function makeDraggable(elem){
 		const position = { x: 0, y: 0 }
-		elem.css({
-			"touch-action": "none",
-			"user-select": "none"
-		});
+		elem.setAttribute("style",
+			"touch-action: none; user-select: none"
+		);
 
-		interact(elem.get(0)).draggable({
+		interact(elem).draggable({
 			listeners: {
 				start (event) {
 					console.log(event.type, event.target);
 					console.log("dragStart");
-					elem.css({
-						"z-index": "99",
-					});
 				},
 				move (event) {
 					console.log("dragging");
@@ -36,16 +25,13 @@ $(document).ready(()=>{
 				},
 				end (event) {
 					console.log("drag end");
-					elem.css({
-						"z-index": "",
-					});
 				}
 			}
 		})
 	}
 
 	function makeDroppable(elem, elemnts2Accept){
-		interact(elem.get(0))
+		interact(elem)
 		.dropzone({
 			accept: elemnts2Accept,
 			ondrop: (event)=>{
@@ -54,7 +40,10 @@ $(document).ready(()=>{
 					+ event.target.getAttribute("class"));
 				console.log(event);
 				}
-		});
+		})
+        .on('dropactivate', function (event) {
+          event.target.classList.add('drop-activated')
+        })
 	}
 
 	//////////////////////////////////////////////////////////////////////////////////
@@ -70,31 +59,13 @@ $(document).ready(()=>{
 		);
 	});
 	///////////////////////////////////////
-	$(document).scroll(function(e){
-		if($(document).scrollTop() < deviceHeight){
-			header.css(
-				{
-					"opacity": 1-($(document).scrollTop()/500),
-					"transform": "translateY("+$(document).scrollTop()/2+"px)"
-				}
-			);
-		}
-		if($(document).scrollTop() > firstTitle.offset().top/1.1){
-			firstTitle.removeClass("hidden").addClass("show");
-			$("#DDinSD p").removeClass("hidden").addClass("show");
-		}
-		if($(document).scrollTop() > secondTitle.offset().top/1.1){
-			secondTitle.removeClass("hidden").addClass("show");
-			$("#DDwithDT p").removeClass("hidden").addClass("show");
-		}
-	});
 
 	for(let i=0; i<imgArray.length; i++){
-		makeDraggable($(imgArray[i]));
+		makeDraggable(imgArray[i]);
 	}
 
 	for(let i=0; i<cardsArray.length; i++){
-		makeDraggable($(cardsArray[i]));
+		makeDraggable(cardsArray[i]);
 	}
 
 	makeDroppable(imgTarget, "img.element2Drag");
