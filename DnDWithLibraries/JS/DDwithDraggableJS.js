edc2681a4943b2d78a11bf5b4362d1499efe5ff8
Marcos Á. Medina <marcmedi1@hotmail.com>
2019-11-22 16:29:12 -0300

    testing drop of draggable library

--- DnDWithLibraries/JS/DDwithDraggableJS.js
+++ DnDWithLibraries/JS/DDwithDraggableJS.js
@@ -4,13 +4,23 @@ window.onload=(()=>{
 	const imgTarget = document.querySelector("#targetStorage");
 	const cardTarget = document.querySelector("#elementTarget");
 	const imgArray = document.querySelectorAll("#startStorage img.element2Drag");
-	const cardsArray = document.querySelector("#elementStart div.card");
+    const cardsArray = document.querySelector("#elementStart div.card");
+
+    
+    const droppable = new Draggable.Droppable(document.querySelectorAll('.elementsContainer'), {
+        draggable: '#startStorage .element2Drag',
+        dropzone: '#targetStorage'
+    });
+      
 
     const draggable = new Draggable.Draggable(document.getElementById("startStorage"), {
         draggable: '#startStorage .element2Drag'
     });
       
-      draggable.on('drag:start', () => console.log('drag:start'));
-      draggable.on('drag:move', () => console.log('drag:move'));
-      draggable.on('drag:stop', () => console.log('drag:stop'));
+    draggable.on('drag:start', () => console.log('drag:start'));
+    draggable.on('drag:move', () => console.log('drag:move'));
+    draggable.on('drag:stop', () => console.log('drag:stop'));
+
+    droppable.on('droppable:dropped', () => console.log('droppable:dropped'));
+    droppable.on('droppable:returned', () => console.log('droppable:returned'));
 })
