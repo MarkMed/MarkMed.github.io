217c17eca5febd24e849460d2a145460b6f6f7c7
Marcos Á. Medina <33690842+MarkMed@users.noreply.github.com>
2020-02-07 13:04:48 -0300

    Code now with comments to explain each step

--- Algorithms and Data Structures Course/FrecuencyCounterPattern.js
+++ Algorithms and Data Structures Course/FrecuencyCounterPattern.js
@@ -1,21 +1,26 @@
 // Code Example of Frecuency Counter Pattern
 function same(arr1, arr2){
-    console.log(`Arr1 Length: ${arr1.length}, Arr2 length: ${arr2.length}`)
-    if(arr1.length !== arr2.length){
-      return false
-    }
-    let frecuencyCounter1 = {};
-    let frecuencyCounter2 = {};
-    for(let i=0; i<arr1.length; i++){
-      frecuencyCounter1[arr1[i]] = (frecuencyCounter1[arr1[i]] || 0) + 1;
-      frecuencyCounter2[arr2[i]] = (frecuencyCounter2[arr2[i]] || 0) + 1;
-    }
-    console.log(frecuencyCounter1);
-    console.log(frecuencyCounter2);
-    for(let key in frecuencyCounter1){
-        if((!(key**2 in frecuencyCounter2))||(frecuencyCounter2[key**2] !== frecuencyCounter1[key])){
-            return false
-        }
-    }
-    return true
+	// console.log(`Arr1 Length: ${arr1.length}, Arr2 length: ${arr2.length}`)
+	if(arr1.length !== arr2.length){
+		// Check for same length of both arrays. False if has not the same length.
+		return false
+	}
+	let freqCount1 = {};
+	let freqCount2 = {};
+	for(let i=0; i<arr1.length; i++){
+		// At this point we understand that both arrays has the same length. So, we can make just ONE for loop to record each item of the array as a key of its obj.
+		freqCount1[arr1[i]] = (freqCount1[arr1[i]] || 0) + 1;
+		freqCount2[arr2[i]] = (freqCount2[arr2[i]] || 0) + 1;
+	}
+	// console.log(freqCount1);
+	// console.log(freqCount2);
+	for(let key in freqCount1){
+		// Check for every key in the 1st obj are in the 2nd one but squared.
+		if((!(key**2 in freqCount2))||(freqCount2[key**2] !== freqCount1[key])){
+			// If the squared key of 1st obj is not in the 2nd obj as a key, the arrays has the same items amount but not the same values (and squared)
+			// OR if the squared key of 1st obj is in the 2nd obj as a key but the amount does not match, the arrays has the same items amount but not the same values (and squared) 
+			return false
+		}
+	}
+	return true
 }
