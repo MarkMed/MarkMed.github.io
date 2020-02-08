0198b6c214f922a564efb3f73d9a0543d93ab5fe
Marcos Á. Medina <marcmedi1@hotmail.com>
2020-02-08 17:39:08 -0300

    refactory of solution

--- Algorithms and Data Structures Course/MultiplePointersExrciseSolution.js
+++ Algorithms and Data Structures Course/MultiplePointersExrciseSolution.js
@@ -3,12 +3,10 @@ function countUniqueValues(sortedArray){
     let counterSrtike = 0;
 
     if(sortedArray.length > 0){
-        counterSrtike++
-        if(sortedArray.length > 1){
-            for(let i=1; i<sortedArray.length; i++){
-                if(sortedArray[i-1] !== sortedArray[i]){
-                    counterSrtike++
-                }
+        counterSrtike++;
+        for(let i=1; i<sortedArray.length; i++){
+            if(sortedArray[i-1] !== sortedArray[i]){
+                counterSrtike++
             }
         }
     }
