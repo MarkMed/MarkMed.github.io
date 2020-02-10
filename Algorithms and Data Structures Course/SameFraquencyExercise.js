66e7fb70bef549b79464305b2372974375c98681
Marcos Á. Medina <marcmedi1@hotmail.com>
2020-02-10 20:40:21 -0300

    solution without comments

--- Algorithms and Data Structures Course/SameFraquencyExercise.js
+++ Algorithms and Data Structures Course/SameFraquencyExercise.js
@@ -15,8 +15,6 @@ function sameFrequency(numb, numb2){
 	console.log(freqCount2);
 	for(let key in freqCount1){
 		if((!(key in freqCount2))||(freqCount2[key] !== freqCount1[key])){
-			// If the squared key of 1st obj is not in the 2nd obj as a key, the arrays has the same items amount but not the same values (and squared)
-			// OR if the squared key of 1st obj is in the 2nd obj as a key but the amount does not match, the arrays has the same items amount but not the same values (and squared) 
 			return false
 		}
 	}
